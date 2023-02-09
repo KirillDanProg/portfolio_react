a0ea3328ee0fc31ed7e6ee81b4b576b6cb4c1582
KirillDanProg <kirill2898v@gmail.com>
2023-02-09 21:55:53 +0300

    telegram link added

--- src/components/main/media-links/MediaLinks.jsx
+++ src/components/main/media-links/MediaLinks.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import commonStyles from "../../../common/commonStyles.module.scss";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import TelegramIcon from '@mui/icons-material/Telegram'
 
 export const MediaLinks = () => {
     return (
@@ -14,6 +15,11 @@ export const MediaLinks = () => {
             <li>
                 <a className={commonStyles.link} href="https://github.com/KirillDanProg?tab=repositories">
                     <GitHubIcon fontSize={"large"}/>
+                </a>
+            </li>
+            <li>
+                <a className={commonStyles.link} href="https://t.me/Kirill_Vychuzhanin">
+                    <TelegramIcon fontSize={"large"}/>
                 </a>
             </li>
         </ul>
