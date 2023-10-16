7f78df33bea7c83fdd44d3b9da57dd0b717568c8
KirillDanProg <kirill2898v@gmail.com>
2023-10-17 00:12:07 +0300

    update: skill cards style

--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -3,14 +3,16 @@ import styles from "./Skills.module.scss";
 
 const Skill = (props) => {
     return (
+        <div className={styles.skillBoxWrapper}>
         <div key={props.id} className={styles.skillBox}>
-           <img alt={"technology image"} src={props.icon}/>
+           <img alt="technology" src={props.icon}/>
             <span className={styles.skillName}>{props.name}</span>
             <ul className={styles.list}>
                 {
                     props.description?.map((el, i) => <li key={i}>{el}</li>)
                 }
             </ul>
+        </div>
         </div>
     );
 };
