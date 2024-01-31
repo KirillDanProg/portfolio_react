09f1a3192311165e483e2f7920018fa802a98dbe
KirillDanProg <kirill2898v@gmail.com>
2024-01-31 12:24:53 +0300

    static data moved from components

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,68 +1,12 @@
 import React from "react";
 import Skill from "./Skill";
 import styles from "./Skills.module.scss";
-import js from "../../assets/img/skillsImg/icons8-javascript.svg";
-import css from "../../assets/img/skillsImg/icons8-css3.svg";
-import redux from "../../assets/img/skillsImg/icons8-redux.svg";
-import react from "../../assets/img/skillsImg/icons8-react.svg";
-import html from "../../assets/img/skillsImg/icons8-html-5.svg";
-import typescript from "../../assets/img/skillsImg/icons8-typescript.svg";
+
 import { SubTitle } from "../../common/components/SubTitle";
 import { Container } from "../../common/components/Container";
+import { skills } from '../../data/skills-data';
 
-const skills = [
-  {
-    id: 4,
-    name: "React",
-    icon: react,
-    description: [
-      " Hooks",
-      " Props",
-      " Class Components",
-      " Router",
-      " Context",
-    ],
-  },
-  {
-    id: 5,
-    name: "Redux",
-    icon: redux,
-    description: ["Toolkit", "RTK Query", "Thunk"],
-  },
-  {
-    id: 3,
-    name: "TypeScript",
-    icon: typescript,
-    description: [" Union Types", " Utility Types", " Enums"],
-  },
 
-  {
-    id: 0,
-    name: "HTML",
-    icon: html,
-    description: [
-      " Semantic Elements",
-      " Block and Inline Elements",
-      " Responsive Web Design",
-    ],
-  },
-  {
-    id: 1,
-    name: "CSS",
-    icon: css,
-    description: [
-      " Layout - The position Property",
-      " Layout - The position Property",
-      " Flexbox and Greed",
-    ],
-  },
-  {
-    id: 2,
-    name: "JavaScript",
-    icon: js,
-    description: [" ES6+", " Scope", " Hoisting", " Closures", " Async"],
-  },
-];
 
 const mappedSkills = skills.map((skill) => {
   return (
--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -1,67 +1,91 @@
-import social from "../assets/img/projectsImg/social.png"
-import todo from "../assets/img/projectsImg/pwa-todo.png"
-import cards from "../assets/img/projectsImg/learning-cards.png"
-import hotels from "../assets/img/projectsImg/hotels.png"
-import telegramBot from "../assets/img/projectsImg/telegramBot.png"
-import moneyTracker from "../assets/img/projectsImg/money-tracker.png"
+import social from "../assets/img/projectsImg/social.png";
+import book from "../assets/img/projectsImg/book-app.png";
+import cards from "../assets/img/projectsImg/learning-cards.png";
+import hotels from "../assets/img/projectsImg/hotels.png";
+import telegramBot from "../assets/img/projectsImg/telegramBot.png";
+import moneyTracker from "../assets/img/projectsImg/money-tracker.png";
 
 export const projects = [
   {
-      id: 4,
-      projectName: "ChatGPT telegram bot",
-      img: telegramBot,
-      repo: "https://github.com/KirillDanProg/gpt-telegram-bot",
-      link: "https://t.me/ChatGPTKiraBot",
-      description:
-          <div>
-              This advanced bot integrates with the OpenAI API to offer:
-              <ul>
-              <li>1) Russian-to-English translation</li>
-              <li>2) Image generation</li>
-              <li>3) Dynamic conversational abilities powered by ChatGPT</li>
-              </ul>
-          </div>,
-      mobile: true
+    id: 4,
+    projectName: "ChatGPT telegram bot",
+    img: telegramBot,
+    repo: "https://github.com/KirillDanProg/gpt-telegram-bot",
+    link: "https://t.me/ChatGPTKiraBot",
+    description: (
+      <div>
+        This advanced bot integrates with the OpenAI API to offer:
+        <ul>
+          <li>1. Russian-to-English translation</li>
+          <li>2. Image generation</li>
+          <li>3. Dynamic conversational abilities powered by ChatGPT</li>
+        </ul>
+      </div>
+    ),
+    mobile: true,
+  },
+  {
+    id: 1,
+    projectName: "Book-search app",
+    img: book,
+    repo: "https://github.com/KirillDanProg/book-app.git",
+    link: "https://kirilldanprog.github.io/book-app/",
+    description: (
+      <div>
+        <strong>Stack: Vanilla JS / rollup / classes</strong>
+        <ul>
+          <li>Functionality</li>
+          <li>1. Books search by title, author, or genre.</li>
+          <li>2. Results Display</li>
+          <li>3. Pagination </li>
+          <li>4. Detail View </li>
+          <li>5. Favorite Books</li>
+          <li>6. User Feedback</li>
+          <li>7. Responsive Design</li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    id: 3,
+    projectName: "Hotel search app",
+    img: hotels,
+    repo: "https://github.com/KirillDanProg/hotel-search-app",
+    link: "https://hotel-search-app.vercel.app",
+    description: (
+      <div>
+        <strong>Stack:</strong>
+        <ul>
+          <li>1. React</li>
+          <li>2. TypeScript</li>
+          <li>3. Redux Toolkit</li>
+          <li>4. Material-UI Core</li>
+          <li>5. SASS</li>
+        </ul>
+      </div>
+    ),
   },
   {
     id: 5,
     projectName: "Money-tracker",
     img: moneyTracker,
     repo: "https://github.com/KirillDanProg/next-app-tracker",
-    description:
-           "Next.js app for expenses tracking with google autentication"
-},
-  {
-      id: 2,
-      projectName: "Cards-learning",
-      img: cards,
-      repo: "https://github.com/KirillDanProg/cards-friday-project",
-      link: "https://cards-friday-project.vercel.app",
-      description: "Educational app made with React / RTK Query / MUI"
+    description: "Next.js app for expenses tracking with google autentication",
   },
   {
-      id: 1,
-      projectName: "PWA-Todolist",
-      img: todo,
-      repo: "https://github.com/KirillDanProg/pwa-todolist.git",
-      link: "https://kirilldanprog.github.io/pwa-todolist/",
-      description: "Todo list app made with React/Redux toolkit"
+    id: 2,
+    projectName: "Cards-learning",
+    img: cards,
+    repo: "https://github.com/KirillDanProg/cards-friday-project",
+    link: "https://cards-friday-project.vercel.app",
+    description: "Educational app made with React / RTK Query / MUI",
   },
   {
-      id: 0,
-      projectName: "Social-network",
-      img: social,
-      repo: "https://github.com/KirillDanProg/social-network.git",
-      // link: "https://kirilldanprog.github.io/social-network/",
-      description: "App made with React with use of Rest API"
+    id: 0,
+    projectName: "Social-network",
+    img: social,
+    repo: "https://github.com/KirillDanProg/social-network.git",
+    // link: "https://kirilldanprog.github.io/social-network/",
+    description: "App made with React with use of Rest API",
   },
-  {
-      id: 3,
-      projectName: "Hotel search app",
-      img: hotels,
-      repo: "https://github.com/KirillDanProg/hotel-search-app",
-      link: "https://hotel-search-app.vercel.app",
-      description: "App for hotel searching"
-  },
-
-]
+];
