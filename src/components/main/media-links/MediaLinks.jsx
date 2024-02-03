9f75decca3b8cec4ed21468e24d80c502e33295d
KirillDanProg <kirill2898v@gmail.com>
2024-02-03 23:46:05 +0300

    refactor: target blank added to media links

--- src/components/main/media-links/MediaLinks.jsx
+++ src/components/main/media-links/MediaLinks.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
+import React from "react";
 import commonStyles from "../../../common/commonStyles.module.scss";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import TelegramIcon from '@mui/icons-material/Telegram'
+import TelegramIcon from "@mui/icons-material/Telegram";
 
 export const MediaLinks = () => {
-    return (
-        <ul className={commonStyles.links}>
-            <li>
-                <a className={commonStyles.link} href="https://instagram.com/kirill_vychuzhanin?igshid=MWI4MTIyMDE=">
-                    <InstagramIcon fontSize={"large"}/>
-                </a>
-            </li>
-            <li>
-                <a className={commonStyles.link} href="https://github.com/KirillDanProg?tab=repositories">
-                    <GitHubIcon fontSize={"large"}/>
-                </a>
-            </li>
-            <li>
-                <a className={commonStyles.link} href="https://t.me/Kirill_Vychuzhanin">
-                    <TelegramIcon fontSize={"large"}/>
-                </a>
-            </li>
-        </ul>
-    );
+  return (
+    <ul className={commonStyles.links}>
+      <li>
+        <a
+          target='_blank'
+          rel='noreferrer'
+          className={commonStyles.link}
+          href='https://instagram.com/kirill_vychuzhanin?igshid=MWI4MTIyMDE='
+        >
+          <InstagramIcon fontSize={"large"} />
+        </a>
+      </li>
+      <li>
+        <a
+          target='_blank'
+          rel='noreferrer'
+          className={commonStyles.link}
+          href='https://github.com/KirillDanProg?tab=repositories'
+        >
+          <GitHubIcon fontSize={"large"} />
+        </a>
+      </li>
+      <li>
+        <a
+          target='_blank'
+          rel='noreferrer'
+          className={commonStyles.link}
+          href='https://t.me/Kirill_Vychuzhanin'
+        >
+          <TelegramIcon fontSize={"large"} />
+        </a>
+      </li>
+    </ul>
+  );
 };
-
