56568dd8bcb6818081b252b93595652a79f1caa3
KirillDanProg <kirill2898v@gmail.com>
2024-02-23 23:11:00 +0300

    feat: particles removed

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,42 +1,55 @@
 import React from "react";
 import styles from "./Main.module.scss";
 import commonStyle from "../../common/commonStyles.module.scss";
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import particlesConfig from "../../assets/particlesjs-config.json";
-import { Container } from "../../common/components/Container";
 import CV from "../../assets/cv/Kirill-frontend-CV.pdf";
 import { MediaLinks } from "./media-links/MediaLinks";
+import { motion } from "framer-motion";
 
 export const Main = () => {
-  const particlesInit = useCallback(async (engine) => {
-    await loadFull(engine);
-  }, []);
-
+  const titleTransition = { ease: "easeOut", duration: 0.7 };
+  const generateAnimation = (x, opacity) => ({ x, opacity });
   return (
-    <Container className={commonStyle.container}>
-      <Particles
-        init={particlesInit}
-        options={particlesConfig}
-        className={styles.particles}
-      />
-
-      <div className={styles.main}>
-        <h4 className={styles.subTitle}>hello & welcome</h4>
-        <h1 className={`${commonStyle.title} ${styles.title}`}>
-          <span className={styles.title_up}>i'm Kirill Vychuzhanin</span>
-          <span className={styles.title_down}>i'm a front-end developer</span>
-        </h1>
-        <MediaLinks />
-        <a
-          href={CV}
-          download='Kirill Front-end CV'
-          className={commonStyle.button}
+    <div id="#main" className={styles.main}>
+      <motion.div
+        animate={{ scale: 1, opacity: 1 }}
+        initial={{ scale: 0.8, opacity: 0 }}
+        transition={{ ease: "easeOut", duration: 0.7 }}
+        className={styles.img}
+      ></motion.div>
+      <div className={styles.content}>
+        <motion.h3
+          className={styles.subTitle}
+          animate={generateAnimation(0, 1)}
+          initial={generateAnimation(200, 0)}
+          transition={titleTransition}
         >
-          download <span className={styles.spanCv}>cv</span>
-        </a>
+          front-end developer
+        </motion.h3>
+        <motion.h1
+          className={styles.title}
+          animate={generateAnimation(0, 1)}
+          initial={generateAnimation(-300, 0)}
+          transition={titleTransition}
+        >
+          Kirill Vychuzhanin
+        </motion.h1>
+
+        <motion.div
+          className={styles.footer}
+          animate={{ y: 0, opacity: 1 }}
+          initial={{ y: 200, opacity: 0 }}
+          transition={{ ease: "circOut", duration: 0.7, delay: 0.2 }}
+        >
+          <MediaLinks />
+          <a
+            href={CV}
+            download="Kirill Front-end CV"
+            className={commonStyle.button}
+          >
+            download <span className={styles.spanCv}>cv</span>
+          </a>
+        </motion.div>
       </div>
-    </Container>
+    </div>
   );
 };
