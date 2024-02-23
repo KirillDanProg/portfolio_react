6f479b6f5eb2841bc458f4c5c2455c739179a3ef
KirillDanProg <kirill2898v@gmail.com>
2024-02-23 23:16:50 +0300

    refactoring: styles, linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import './App.scss';
-import {Main} from "./components/main/Main";
+import "./App.scss";
+import { Main } from "./components/main/Main";
 import Skills from "./components/skills/Skills";
 import Projects from "./components/projects/Projects";
 import Contacts from "./components/contacts/Contacts";
-import {Footer} from "./components/footer/Footer";
+import { Footer } from "./components/footer/Footer";
+import { Header } from "./components/header/Header";
 
 function App() {
-
-    return (
-            <div className="App">
-                {/*<Header/>*/}
-                <Main/>
-                <Skills/>
-                <Projects/>
-                <Contacts/>
-                <Footer/>
-            </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <Main />
+        <Skills />
+        <Projects />
+        <Contacts />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
