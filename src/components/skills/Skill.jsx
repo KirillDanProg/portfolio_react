324d36751a70d86811915a2aa56a37bb8a5b9465
KirillDanProg <kirill2898v@gmail.com>
2024-02-24 14:14:10 +0300

    refactor: ref and useInView hook moved inside of skill

--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -1,35 +1,31 @@
 import React from "react";
 import { motion } from "framer-motion";
 import styles from "./Skills.module.scss";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 
-const Skill = ({
-  id,
-  icon,
-  name,
-  description,
-  ref,
-  isInView,
-  ...restProps
-}) => {
+const Skill = ({ id, icon, name, description, ...restProps }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
     <motion.div
       initial={{
-        transform: "translateX(2000px)",
         opacity: 0,
       }}
       animate={{
-        transform: isInView ? "translateX(0)" : null,
-        opacity: 1,
+        opacity: isInView ? 1 : 0,
       }}
       transition={{
-        duration: 1.4,
+        duration: 1.5,
+        ease: "circInOut",
         delay: 0.2,
       }}
       className={styles.skillBoxWrapper}
       {...restProps}
     >
-      <div key={id} className={styles.skillBox}>
-        <img alt="technology" src={icon} />
+      <div key={id} className={styles.skillBox} ref={ref}>
+        <img alt="technology" className={styles.icon} src={icon} />
         <span className={styles.skillName}>{name}</span>
         <ul className={styles.list}>
           {description?.map((el, i) => (
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,16 +1,12 @@
-import React, { useRef } from "react";
+import React from "react";
 import Skill from "./Skill";
 import styles from "./Skills.module.scss";
 
 import { SubTitle } from "../../common/components/SubTitle";
 import { Container } from "../../common/components/Container/Container";
 import { skills } from "../../data/skills-data";
-import { useInView } from "framer-motion";
 
 const Skills = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-
   const mappedSkills = skills.map((skill) => {
     return (
       <Skill
@@ -18,14 +14,13 @@ const Skills = () => {
         icon={skill.icon}
         name={skill.name}
         description={skill.description}
-        isInView={isInView}
       />
     );
   });
   return (
     <Container>
       <SubTitle>my top skills</SubTitle>
-      <div id="#skills" className={styles.skillsBox} ref={ref}>
+      <div id="#skills" className={styles.skillsBox}>
         {mappedSkills}
       </div>
     </Container>
